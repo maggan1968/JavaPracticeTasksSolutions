f380f971d28b5059156e28abba9d724aea03bae8
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-11-15 10:54:18 -0500

    Java Practice Tasks

--- src/day09_IfElse_MultiBranchIf/SalaryCalculator.java
+++ src/day09_IfElse_MultiBranchIf/SalaryCalculator.java
@@ -6,16 +6,16 @@ public class SalaryCalculator {
         double salary = 75_000;
         boolean isMarried = true;
 
-        double taxRate = 0;
+        double taxRate = 0 ;
 
         if(salary>= 130000 ){ //false: salary < 130000;
-            taxRate = 0.35;
+            taxRate = 0.35; //35%
         }else if(salary>= 100000 ){ //false: salary < 100000;
-            taxRate = 0.30;
+            taxRate = 0.30; //30%
         }else if(salary>= 80000 ){ //false: salary < 80000
-            taxRate = 0.25;
+            taxRate = 0.25; //25%
         }else {
-            taxRate = 0.20;
+            taxRate = 0.20; //20%
         }
 
 
