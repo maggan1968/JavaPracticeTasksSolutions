331271267c98b8b01f180dd65b45d024a80da687
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-11-15 10:48:07 -0500

    Java Practice Tasks

--- src/day09_IfElse_MultiBranchIf/SalaryCalculator.java
+++ src/day09_IfElse_MultiBranchIf/SalaryCalculator.java
@@ -8,11 +8,11 @@ public class SalaryCalculator {
 
         double taxRate = 0;
 
-        if(salary>= 130000 ){
+        if(salary>= 130000 ){ //false: salary < 130000;
             taxRate = 0.35;
-        }else if(salary>= 100000 ){
+        }else if(salary>= 100000 ){ //false: salary < 100000;
             taxRate = 0.30;
-        }else if(salary>= 80000 ){
+        }else if(salary>= 80000 ){ //false: salary < 80000
             taxRate = 0.25;
         }else {
             taxRate = 0.20;
@@ -33,10 +33,10 @@ public class SalaryCalculator {
 /*
 2. Write a program that can calculate the salary after tax based on the following requirements
                 the tax rates are:
-                        35% for salary of 130K or more
-      			30% for salary of 100K to 130k (excluded)
-      			25% for salary of 80K to 100K (excluded)
-      			20% for salary less than 80K
+                    35% for salary of 130K or more
+                    30% for salary of 100K to 130k (excluded)
+                    25% for salary of 80K to 100K (excluded)
+                    20% for salary less than 80K
 
                 in addition, if the person is married, he/she will pay 5% less tax
  */
