ff64180bd177474012e8b14917410c9cb8342153
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-11-14 22:57:13 -0500

    Java Practice Tasks

--- src/day10_NestedIf_Ternaries/Grade.java
+++ src/day10_NestedIf_Ternaries/Grade.java
@@ -34,3 +34,15 @@ public class Grade {
     }
 
 }
+
+/*
+3. Create a class called Grade, a char variable named grade is given. write a program to print the following messages:
+            'A': excellent
+            'B': great job
+            'C': good
+            'D': passed
+            'F': failed
+            other wise: invalid
+
+            NOTE: MUST USE NESTED IF. DO NOT USE MORE THAN ONE PRINT STATEMENT
+ */
