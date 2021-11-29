00896bfb3277c7599cd40763605ba177888f6b13
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-11-28 20:37:17 -0500

    Java Practice Tasks

--- src/day16_ForLoop_String/MultiplyTwoNumbers.java
+++ src/day16_ForLoop_String/MultiplyTwoNumbers.java
@@ -7,6 +7,17 @@ public class MultiplyTwoNumbers {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter two positive numbers:");
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+
+        int result = 0;
+
+        for (int i = 0; i < num2; i++) {
+            result += num1;
+        }
+
+        System.out.println("Multiplication = " + result);
 
     }
 
