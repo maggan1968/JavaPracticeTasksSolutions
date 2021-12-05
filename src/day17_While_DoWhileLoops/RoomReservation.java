d801e8eb8e8bad05cae706e15ac29019e3d644d2
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-05 18:46:37 -0500

    Java Practice Tasks

--- src/day17_While_DoWhileLoops/RoomReservation.java
+++ src/day17_While_DoWhileLoops/RoomReservation.java
@@ -8,38 +8,26 @@ public class RoomReservation {
         Scanner scan = new Scanner(System.in);
         int total = 0;
 
-        while(true) {
-            System.out.println("Which room would you like to select?");
-            System.out.println("\t\tKing Bed == $120");
-            System.out.println("\t\tQueen Bed == $100");
-            System.out.println("\t\tSingle Bed == $80");
 
-            String word = scan.nextLine().toLowerCase();
+        System.out.println("Which room would you like to select?");
+        System.out.println("\t\tKing Bed == $120");
+        System.out.println("\t\tQueen Bed == $100");
+        System.out.println("\t\tSingle Bed == $80");
 
-            while (!(word.equals("king bed") || word.equals("queen bed") || word.equals("single bed"))) {
-                System.out.println("Invalid Entry, Please Re-Enter");
-                word = scan.nextLine().toLowerCase();
-            } // to make sure user selected either the king bed, or queen bed or single bed
+        String word = scan.nextLine().toLowerCase();
 
-            total += (word.equals("king bed")) ? 120 : (word.equals("queen bed")) ? 100 : 80;
+        while (!(word.equals("king bed") || word.equals("queen bed") || word.equals("single bed"))) {
+            System.out.println("Invalid Entry, Please Re-Enter");
+            word = scan.nextLine().toLowerCase();
+        } // to make sure user selected either the king bed, or queen bed or single bed
 
-            System.out.println("Would you like to select another room?");
-            String a = scan.nextLine().toLowerCase();
+        total += (word.equals("king bed")) ? 120 : (word.equals("queen bed")) ? 100 : 80;
 
-            while( !( a.equals("yes") || a.equals("no") ) ){ // while the answer is not valid answer
-                System.out.println("Invalid Entry, please re-enter");
-                System.out.println("Would you like to select another room?");
-                a = scan.nextLine().toLowerCase();
-            } // it stops if the answer is either ye or no
+        System.out.println("Your total price is: $" + total);
 
-            if(a.equals("no")){
-                System.out.println("Your total price is: $"+total);
-                break;
-            }
+        scan.close();
 
-            scan.close();
 
-        }
     }
 
 
