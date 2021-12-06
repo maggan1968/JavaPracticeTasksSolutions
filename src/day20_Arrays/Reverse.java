adda4af93f278b0fa57e20d62eceab21b41fd2e8
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-12-05 19:49:04 -0500

    Java Practice Tasks

--- src/day20_Arrays/Reverse.java
+++ src/day20_Arrays/Reverse.java
@@ -1,11 +1,20 @@
 package day20_Arrays;
 
+import java.util.Arrays;
+
 public class Reverse {
 
     public static void main(String[] args) {
 
+        int[] array = {1,2,3,4,5};
 
+        int[] reversedArray = new int[array.length];
 
+        for(int i = array.length-1, j=0; i >= 0 ; i--, j++){
+            reversedArray[j] = array[i];
+        }
+
+        System.out.println( Arrays.toString(reversedArray) );
 
     }
 
